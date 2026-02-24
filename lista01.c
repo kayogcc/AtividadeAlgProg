27723ee737ff06d3d4b8066121731d77b1cba070
Kayo Camargo <kayo.gcc1@puccampinas.edu.br>
2026-02-24 14:38:59 -0300

    Add files via upload

---
+++ lista01.c
@@ -0,0 +1,41 @@
+// LISTA DE EXERCÍCIOS 01 - Entrada, Saída e Operadores em C
+// Alunos: RA: 25022660 - Kayo Gabriel Casatti Camargo
+// Alunos: RA:  - Yago Campos Neves
+
+
+
+#include <stdio.h>
+#include <math.h>
+
+
+void exercicio01() 
+{
+    printf("\nExercício 01:\n\n");
+    printf("Hello World!\n");
+}
+
+void exercicio02() 
+{
+    //....//
+}
+
+
+int main() {
+
+    int op;
+
+    do {
+        printf("\nEscolha o número do exercício: ");
+        scanf("%d", &op);
+
+        switch (op) {
+            case 1: exercicio01(); break;
+            // case 2: exercicio02(); break;
+            // case 3: exercicio03(); break;
+            /* ... */
+        }
+    } while (op != 0);
+
+    return 0;
+}
+
